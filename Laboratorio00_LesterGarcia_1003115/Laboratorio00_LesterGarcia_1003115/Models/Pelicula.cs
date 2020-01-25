43cdff635d04cd873fd76928118c5d9b4a5fecce
Lester Andrés García Aquino <lagarcia2015@gmail.com>
2020-01-24 18:51:43 -0600

    Agregando modelo Película

---
+++ Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio00_LesterGarcia_1003115.Models
+{
+    public class Pelicula
+    {
+        //Campos los cuales ser+an requeridos en el método POST
+        public int numero { get; set; }
+        public string nombre { get; set; }
+        public int anio { get; set; }
+        public string director { get; set; }
+    }
+}

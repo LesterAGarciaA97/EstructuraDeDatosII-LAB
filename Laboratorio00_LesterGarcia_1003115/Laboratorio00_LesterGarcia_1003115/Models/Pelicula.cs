c6070a532305854574cf2d88a4bedf954198e830
Lester Andrés García Aquino <lagarcia2015@gmail.com>
2020-01-25 18:23:08 -0600

    Modificando README.md y agregando enunciado del laboratorio en formato PDF

--- Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
+++ Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
@@ -28,16 +28,16 @@ namespace Laboratorio00_LesterGarcia_1003115.Models
         //Datos quemados de forma predeterminada en el programa
         private static Pelicula[] listaPeliculas =
         {
-            new Pelicula {numero = 1, director ="Zack Snyder", nombre = "Batman vs Superman", anio = 2016},
-            new Pelicula {numero = 2, director ="Anthony & Joe Russo", nombre = "Avengers: Infinity War", anio = 2018},
-            new Pelicula {numero = 3, director ="Anthony & Joe Russo", nombre = "Avengers: Endgame", anio = 2019},
-            new Pelicula {numero = 4, director ="Chris Wedge & Carlos Saldanha", nombre = "La era de hielo", anio = 2002},
-            new Pelicula {numero = 5, director ="Rob Minkoff & Roger Allers", nombre = "El rey león", anio = 1994},
-            new Pelicula {numero = 6, director ="Andrés Muschietti", nombre = "IT", anio = 2017},
-            new Pelicula {numero = 7, director ="John Leonetti", nombre = "Annabelle", anio = 2014},
-            new Pelicula {numero = 8, director ="Rob Letterman", nombre = "Pokémon: Detective Pikachu", anio = 2019},
-            new Pelicula {numero = 9, director ="Guy Ritchie", nombre = "Aladdín", anio = 2019},
-            new Pelicula {numero = 10, director ="Cate Shortland", nombre = "Black Widow", anio = 2020}
+            new Pelicula {numero = 0, director ="Zack Snyder", nombre = "Batman vs Superman", anio = 2016},
+            new Pelicula {numero = 1, director ="Anthony & Joe Russo", nombre = "Avengers: Infinity War", anio = 2018},
+            new Pelicula {numero = 2, director ="Anthony & Joe Russo", nombre = "Avengers: Endgame", anio = 2019},
+            new Pelicula {numero = 3, director ="Chris Wedge & Carlos Saldanha", nombre = "La era de hielo", anio = 2002},
+            new Pelicula {numero = 4, director ="Rob Minkoff & Roger Allers", nombre = "El rey león", anio = 1994},
+            new Pelicula {numero = 5, director ="Andrés Muschietti", nombre = "IT", anio = 2017},
+            new Pelicula {numero = 6, director ="John Leonetti", nombre = "Annabelle", anio = 2014},
+            new Pelicula {numero = 7, director ="Rob Letterman", nombre = "Pokémon: Detective Pikachu", anio = 2019},
+            new Pelicula {numero = 8, director ="Guy Ritchie", nombre = "Aladdín", anio = 2019},
+            new Pelicula {numero = 9, director ="Cate Shortland", nombre = "Black Widow", anio = 2020}
         };
         public Stack<Pelicula> listadoPeliculas = new Stack<Pelicula>(listaPeliculas);
     }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EstructuraDeDatosII-LAB
 Laboratorio de estructura de datos II
+## Laboratorio 00 - Repaso
+Este laboratorio tiene como objetivo la creación de una API en .NET Core con la funcionalidad básica
+de los métodos GET y POST utilizando una lista como estructura de datos predeterminada.

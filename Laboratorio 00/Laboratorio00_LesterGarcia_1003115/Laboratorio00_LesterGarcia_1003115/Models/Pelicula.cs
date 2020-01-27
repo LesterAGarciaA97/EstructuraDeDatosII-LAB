65954e3298fbed0c5d77992bd61611d4b64d9b21
Lester Andrés García Aquino <lagarcia2015@gmail.com>
2020-01-26 22:23:26 -0600

    Se vació la lista incial para recibir datos desde 0

--- Laboratorio 00/Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
+++ Laboratorio 00/Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
@@ -25,19 +25,9 @@ namespace Laboratorio00_LesterGarcia_1003115.Models
                 }
             }
         }
-        //Datos quemados de forma predeterminada en el programa
+        //La lista al iniciar el progarma está vacía, irá reciendo los datos conforme el usuario los ingrese
         private static Pelicula[] listaPeliculas =
         {
-            new Pelicula {numero = 0, director ="Zack Snyder", nombre = "Batman vs Superman", anio = 2016},
-            new Pelicula {numero = 1, director ="Anthony & Joe Russo", nombre = "Avengers: Infinity War", anio = 2018},
-            new Pelicula {numero = 2, director ="Anthony & Joe Russo", nombre = "Avengers: Endgame", anio = 2019},
-            new Pelicula {numero = 3, director ="Chris Wedge & Carlos Saldanha", nombre = "La era de hielo", anio = 2002},
-            new Pelicula {numero = 4, director ="Rob Minkoff & Roger Allers", nombre = "El rey león", anio = 1994},
-            new Pelicula {numero = 5, director ="Andrés Muschietti", nombre = "IT", anio = 2017},
-            new Pelicula {numero = 6, director ="John Leonetti", nombre = "Annabelle", anio = 2014},
-            new Pelicula {numero = 7, director ="Rob Letterman", nombre = "Pokémon: Detective Pikachu", anio = 2019},
-            new Pelicula {numero = 8, director ="Guy Ritchie", nombre = "Aladdín", anio = 2019},
-            new Pelicula {numero = 9, director ="Cate Shortland", nombre = "Black Widow", anio = 2020}
         };
         public Stack<Pelicula> listadoPeliculas = new Stack<Pelicula>(listaPeliculas);
     }

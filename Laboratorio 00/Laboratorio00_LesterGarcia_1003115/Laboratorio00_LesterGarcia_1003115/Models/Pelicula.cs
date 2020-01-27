cc184006564e6160c61841c2c358c1abcd36ed06
Lester Andrés García Aquino <lagarcia2015@gmail.com>
2020-01-26 22:26:13 -0600

    Se cambió el parámetro 'número' por 'id'

--- Laboratorio 00/Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
+++ Laboratorio 00/Laboratorio00_LesterGarcia_1003115/Laboratorio00_LesterGarcia_1003115/Models/Pelicula.cs
@@ -5,7 +5,7 @@ namespace Laboratorio00_LesterGarcia_1003115.Models
     public class Pelicula
     {
         //Campos los cuales serán requeridos en el método POST
-        public int numero { get; set; }
+        public int id { get; set; }
         public string nombre { get; set; }
         public int anio { get; set; }
         public string director { get; set; }
